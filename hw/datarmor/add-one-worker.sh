55d6d79891b7f5cba03e1c680b376f3920cd903a
Aurelien @LPO <aurelien.junk@gmail.com>
2017-11-19 08:44:33 +0100

    Refresh dask cluster init

--- hw/datarmor/add-one-worker.sh
+++ hw/datarmor/add-one-worker.sh
@@ -1,11 +1,11 @@
 #!/bin/csh
 # Configure these values to change the size of your dask cluster
-#PBS -N dask
-#PBS -q mpi_2
-#PBS -l select=2:ncpus=28:mpiprocs=7:ompthreads=7
+#PBS -N dworker
+#PBS -q mpi_1
+#PBS -l select=1:ncpus=28:mpiprocs=7:ompthreads=7
 #PBS -l walltime=24:00:00
 
-# for infos about the queue: qstat -Qf mpi_2
+# for infos about the queue: qstat -Qf mpi_1
 
 # get the path right
 setenv PATH ${HOME}/.miniconda3/envs/pangeo/bin:${PATH}
@@ -14,9 +14,8 @@ setenv PATH ${HOME}/.miniconda3/envs/pangeo/bin:${PATH}
 # go into directory where job was launched
 cd $PBS_O_WORKDIR
 
-rm -f scheduler.json
-mpirun --np 14 dask-mpi --nthreads 4 --memory-limit 5e9 --interface ib0 
+mpirun --np 7 dask-mpi --nthreads 4 --memory-limit 5e9 --interface ib0 --no-scheduler --local-directory ${HOME}
+
 # this requests 5Go per MPI process
 # datarmor has 128Go per node, 128/7 = 18Go per mpi process
-
 
