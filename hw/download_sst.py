daaf45028a4fc369be1f998bd2dd4f551df7e624
aurelien <aurelien.junk@gmail.com>
2018-02-02 16:35:19 +0100

    Refresh download sst with xarray , very cool git status!

--- hw/download_sst.py
+++ hw/download_sst.py
@@ -45,109 +45,43 @@
 
 # Import libraries
 import os, sys
-import urllib
-#from urllib.error import URLError, HTTPError
-#from urllib import error
-#import urllib2
+from optparse import OptionParser
 import ftplib as ftplib
 import numpy as np
-import matplotlib.pyplot as plt
-from  matplotlib.dates import date2num, datetime, num2date
-from netCDF4 import Dataset
+import datetime
+#from netCDF4 import Dataset
+import xarray as xr
 
 
-#dpath = '/Users/aponte/Current_projects/anr_jcjc/work/data/'
-dpath = '/home/slyne/aponte/sst_fast/data_nwa_06/'; bbox_name = 'NWA_06'
-#dpath = '/home/slyne/aponte/sst_fast/data_nwa_08/'; bbox_name = 'NWA_08'
-#dpath = '/home/slyne/aponte/sst_fast/data_nwa_09/'; bbox_name = 'NWA_09'
-
-# stdbuf -oL python himawari.py >& hw_nwa_06.log
-# stdbuf -oL python himawari.py >& hw_nwa_08.log
-# stdbuf -oL python himawari.py >& hw_nwa_09.log
-
-# reference time for netcdf file
-t0 = date2num(datetime.datetime(2015,7,1,0,0,0))
 
 #
-# download data
+# Parse inputs
 #
-def readwrite_hw(time, lon, lat, bbox=None, first=False):
-
-    # format date and time
-    YYYYMM = time.strftime('%Y%m')
-    DD = time.strftime('%d')
-    YYYYMMDDhhmm = time.strftime('%Y%m%d%H%M')
-
-    # SST data, nc format
-    #VER='v100'; VERd='v1.0'
-    VER='v101'; VERd='v1.1'
-    FVER='01.0'
-
-    # filename
-    fdir='/pub/himawari/L2/SST/'+VER+'_nc4_normal_nrt/'+YYYYMM+'/'+DD+'/'
-    fname=YYYYMMDDhhmm+'00-JAXA-L2P_GHRSST-SSTskin-H08_AHI_NRT-'+VERd+'-v02.0-fv'+FVER+'.nc'
-    fname_bbox = YYYYMMDDhhmm+'_'+bbox_name+'.nc'
-
-    # open ftp
-    host, login, passwd = ftp_login()
-    ftp = ftplib.FTP(host)
-    ftp.login(login, passwd)
-
-    if not os.path.isfile(dpath+fname_bbox):
-        print 'No data file found, download from jaxa ftp: '+fname
-        file = open(dpath+fname,'wb')
-        #
-        ftp = ftplib.FTP(host)
-        ftp.login(login, passwd)
-        ftp.cwd(fdir)
-        try:
-            ftp.retrbinary('RETR %s' % fname, file.write)
-            file.close()
-            ftp.quit()
-        except:
-            print "Error"
-        #attempts=0
-        #while attempts < 3:
-        #   try:
-        #        print 'test0'
-        #        #urllib2.Request(ftp+fdir+fname, dpath+fname)
-        #        urllib.urlretrieve(ftp+fdir+fname, dpath+fname)
-        #        break
-        #    except error.URLError as e:
-        #    #except
-        #        print 'test1'
-        #        attempts += 1
-        #        print ' - Failed attempt '+str(attempts)
-        #        continue
-        #    except HTTPError as e:
-        #        print 'test2'
-    else:
-        print fname_bbox+' is already on disk'
-
-    # very first file or not
-    if first:
-        fname_bbox = fname
-    else:
-        # create bbox file
-        nc_create(dpath+fname_bbox, lon, lat)
-        # load sst data
-        if os.path.getsize(dpath+fname)>1:
-            nc = Dataset(dpath+fname, 'r')
-            sst = nc.variables['sea_surface_temperature'][0, bbox[2]:bbox[3], bbox[0]:bbox[1]]
-            nc.close()
-        else:
-            flog = open(dpath+fname+'.log','wb')
-            flog.write('Issue with this file')
-            flog.close()
-            sst = None
-        # append
-        nc_append(dpath+fname_bbox, sst, time)
-        # delete full data file
-        if sst is not None:
-            os.remove(dpath+fname)
-
-    return dpath+fname_bbox
-
+def parse_input():
+    """ Parse inputs
+    """
+    parser = OptionParser()
+    parser.add_option("-c", "--coordinates", dest="c",
+                      help="coordinate bounds, ex: -c lonmin,lonmax,latmin,latmax)", \
+                      default='105.,125.,-40.,-10')
+    #parser.add_option("-lat", "--latitude", dest="lat",
+    #                  help="latitude bounds", default=[-23., -10])
+    parser.add_option("-n", "--name", dest="name",
+                      help="box name", default='NWA')    
+    parser.add_option("-s", "--timestart", dest="tstart",
+                      help="starting time, ex: -s 2015,9,15,0,0,0", default='2015,9,15,0,0,0')
+    parser.add_option("-d", "--delt", dest="delt",
+                      help="time interval in days", default='10.')
+    (options,args) = parser.parse_args()
+    #
+    c = [float(v) for v in options.c.split(',')]
+    box = {'lon': slice(c[0],c[1]), 'lat': slice(np.max(c[2:]),np.min(c[2:])), 'name': options.name}
+    #
+    tstart = [int(v) for v in options.tstart.split(',')]
+    print(tstart)
+    tstart=datetime.datetime(*tstart)
+    # 
+    return box, tstart, float(options.delt)
 
 def ftp_login():
     ''' Read ftp address, login and password
@@ -159,52 +93,82 @@ def ftp_login():
     f.close()
     return host, login, passwd
 
-
 #
-# create netcdf file where data is appended
+# download data
 #
-def nc_create(fname, lon,lat):
+def download_hw(times,box=None):
 
-    nc = Dataset(fname, 'w', format='NETCDF4_CLASSIC', clobber=True)
+    # open ftp
+    host, login, passwd = ftp_login()
+    ftp = ftplib.FTP(host)
+    ftp.login(login, passwd)
 
-    # create dimensions
-    nc.createDimension('lon', lon.size)
-    nc.createDimension('lat', lat.size)
-    nc.createDimension('t', None)
-
-    # create variables
-    dtype = 'f8'
-    nc_x = nc.createVariable('lon', dtype, ('lon'))
-    nc_y = nc.createVariable('lat', dtype, ('lat'))
-    nc_x[:] = lon
-    nc_y[:] = lat
+    # loop around times
+    sstfiles=[]
+    for time in times:
+        
+        # format date and time
+        YYYYMM = time.strftime('%Y%m')
+        DD = time.strftime('%d')
+        YYYYMMDDhhmm = time.strftime('%Y%m%d%H%M')
+        #
+        #VER='v100'; VERd='v1.0'
+        VER='v101'; VERd='v1.1'
+        FVER='01.0'
+        #
+        fdir='/pub/himawari/L2/SST/'+VER+'_nc4_normal_nrt/'+YYYYMM+'/'+DD+'/'
+        fname_pref=YYYYMMDDhhmm+'00-JAXA-L2P_GHRSST-SSTskin-H08_AHI_NRT-'+ \
+                    VERd+'-v02.0-fv'+FVER+'.nc'
+        fname_out=box['name']+'_'+YYYYMMDDhhmm+'.nc'
+        try:
+            # get ftp file names
+            ftp.cwd(fdir)
+            files_all = ftp.nlst()
+            files = []
+            for f in files_all:
+                if fname_pref in f and f[-2:]=='nc':
+                    files.append(f)
+            #
+            for fname in files:
+                #
+                fileraw = dpath+fname_pref
+                sstfiles.append(fileraw)
+                #
+                if not os.path.isfile(fileraw):
+                    print('No sst file found, download from jaxa ftp: '+fname)
+                    lfile = open(dpath+fname,'wb')
+                    try:
+                        ftp.retrbinary('RETR %s' % fname, lfile.write)
+                    except:
+                        print('Error')
+                    lfile.close()
+                else:
+                    print(fname_pref+' is already on disk')
+                #
+                if box is not None:
+                    try:
+                        process_raw_data(fileraw,box,fname_out)
+                    except:
+                        print('Cannot process '+fileraw)                
+        except:
+            print('Cannot access data in '+fdir)
     #
-    nc.createVariable('t', dtype, ('t'))
-    #
-    nc.createVariable('sst', dtype, ('t', 'lat', 'lon',))
+    ftp.quit()
 
-    nc.close()
+    return sstfiles
 
-    return
 
-#
-# append to netcdf file where data is append
-#
-def nc_append(fname, sst, time):
-
-    # open netcdf file
-    nc = Dataset(fname, 'a', format='NETCDF4_CLASSIC')
-
-    it = nc.variables['sst'].shape[0]
-    nc.variables['t'][it] = date2num(time)-t0
-    if sst is None:
-        nc.variables['sst'][it,...] = 0.
-    else:
-        nc.variables['sst'][it,...] = sst
-
-    nc.close()
-
-    return
+def process_raw_data(fileraw,box,fname_out, \
+                     variables=['sea_surface_temperature','wind_speed','solar_zenith_angle']):
+    """ Open the raw file and zoom into box
+    """
+    ds = xr.open_dataset(fileraw).sel(lon=box['lon'],lat=box['lat'])
+    #print(ds)
+    for v in ds.data_vars:
+        if v not in variables:
+            ds = ds.drop(v)
+    ds.to_netcdf(dpath+fname_out)
+    print('  file reduced to box coordinates')
 
 
 #
@@ -212,43 +176,26 @@ def nc_append(fname, sst, time):
 #
 if __name__ == "__main__":
 
-    # bounding box
-    lonb = [105., 125.]
-    latb = [-23., -10]
 
-    # time line
-    t1=datetime.datetime(2015,6,15,0,0,0); delt = 30. # in days
+    box, tstart, delt = parse_input()
+    print('box defined by following coordinates: ',box)
+    print('start time: '+str(tstart))
+    print('time interval considered: %.1f days'%delt)
+    
+    # output dir
+    dpath = '/home/datawork-lops-osi/data/hw/sst/'+box['name']+'/'
+    if not os.path.isdir(dpath): os.mkdir(dpath)
+
+    # time line, should b
+    #t1=datetime.datetime(2015,6,15,0,0,0); delt = 30. # in days
     # first file is in fact: 201507/07/20150707015000-JAX...
     #t1=datetime.datetime(2015,7,26,0,0,0); delt = 30. # in days
     #t1=datetime.datetime(2015,9,1,0,0,0); delt = 30.
 
-    time = [t1+datetime.timedelta(minutes=10*n) for n in xrange(int(delt*24*60/10))]
+    time = [tstart+datetime.timedelta(minutes=10*n) for n in range(int(delt*24*60/10))]
 
     # download a first file in order to get coordinates
-    fname = readwrite_hw(time[0], None, None, first=True)
-    nc = Dataset(fname,'r')
-    lon = nc.variables['lon'][:]
-    lat = nc.variables['lat'][:]
-    nc.close()
+    sstfiles = download_hw(time[:],box)
 
-    # find bounding box limits
-    ilon1 = np.where(lon>lonb[0])[0][0]
-    ilon2 = np.where( (lon<lonb[1]) & (lon>0.) )[0][-1]
-    ilat1 = np.where(lat<latb[1])[0][0]
-    ilat2 = np.where(lat>latb[0])[0][-1]
-    bbox = [ilon1, ilon2, ilat1, ilat2]
-
-    # download other files and store in one single file
-    fnames = [readwrite_hw(t, lon[ilon1:ilon2], lat[ilat1:ilat2], bbox=bbox) for t in time]
-
-#     for filen, t in zip(fname,time):
-# 
-#         nc = Dataset(filen, 'r')
-#         sst = nc.variables['sea_surface_temperature'][0, ...]
-#         nc.close()
-# 
-#         plt.figure(figsize=(10,10))
-#         plt.pcolormesh(sst)
-#         plt.savefig('figs/sst_'+t.strftime('%Y%m%d%H%M')+'.png',dpi=300)
 
 
