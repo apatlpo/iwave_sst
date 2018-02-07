5d911d58a34f5a61479b152e820a22c7192ee573
Aurelien @LPO <aurelien.junk@gmail.com>
2018-02-07 20:21:53 +0100

    add hret analysis

---
+++ tides/utils.py
@@ -0,0 +1,45 @@
+''' Utils for tidal processing
+'''
+
+import numpy as np
+import xarray as xr
+import dask.array as da
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+#from cmocean import cm
+import time
+
+g=9.81
+cpd=2.*np.pi/86400. # radian/s
+
+#------------------------------ plots ---------------------------------------
+
+def grad(d,lon=None,lat=None,mask=None):
+    """ Compute the gradient of data on lon/lat grid
+    Should take mask as input
+    """
+    if lon is None:
+        lon = 'longitude'
+    if lat is None:
+        lat = 'latitude'
+    dx = ndiff(d,lon)
+    dy = ndiff(d,lat)
+    return dx, dy
+
+def ndiff(d,c):
+    c0 = d[c].values
+    di = d.diff(c,label='lower')/d[c].diff(c,label='lower')
+    di = (di + di.roll(**{c:1}))*.5
+    return di
+
+def mom_inv(dvdx,dvdy,f,o):
+    i = np.complex(0.,1.)
+    uc = -g * (i*o*dvdx-f*dvdy)/(o**2-f**2)
+    vc = -g * (f*dvdx+i*o*dvdy)/(o**2-f**2)
+    return uc, vc
+
+def ri2c(r,i):
+    return r+np.complex(0,1.)*i
+
+def c2ri(c):
+    return np.real(c), np.imag(c)
