cc13df246903523dca7c705cd50fa81bc44be053
Aurelien @LPO <aurelien.junk@gmail.com>
2018-02-11 16:57:52 +0100

    add drag coefficient to hret velocities, improvement dateline treatment, add zooms

--- tides/utils.py
+++ tides/utils.py
@@ -12,30 +12,47 @@ import time
 g=9.81
 cpd=2.*np.pi/86400. # radian/s
 
-#------------------------------ plots ---------------------------------------
+#------------------------------ gradients ---------------------------------------
 
-def grad(d,lon=None,lat=None,mask=None):
+def grad(d, lon='longitude', lat='latitude'):
     """ Compute the gradient of data on lon/lat grid
-    Should take mask as input
     """
-    if lon is None:
-        lon = 'longitude'
-    if lat is None:
-        lat = 'latitude'
-    dx = ndiff(d,lon)/np.cos(np.pi/180.*d[lat])
-    dy = ndiff(d,lat)
+    dx = diff_lonlat(d,lon)
+    dy = diff_lonlat(d,lat)
     return dx, dy
 
-def ndiff(d,c):
-    #c0 = d[c].values
-    di = d.diff(c,label='lower')/d[c].diff(c,label='lower')/(111.e3)
+def diff_lonlat(d, c):
+    """ Compute the gradient of a variable laid out on a regular lon/lat grid
+    """
+    if c is 'longitude':
+        # treats dateline data correctly
+        di = lon_extension(d)
+    else:
+        di = d   
+    #
+    di = di.diff(c,label='lower')/di[c].diff(c,label='lower')/(111.e3)
     di = (di + di.roll(**{c:1}))*.5
+    #
+    if c is 'longitude':
+        di = di/np.cos(np.pi/180.*di['latitude'])
     return di
 
-def mom_inv(dvdx,dvdy,f,o):
-    i = np.complex(0.,1.)
-    uc = -g * (i*o*dvdx-f*dvdy)/(o**2-f**2)
-    vc = -g * (f*dvdx+i*o*dvdy)/(o**2-f**2)
+def lon_extension(v):
+    """ Extends data array longitudinally in order to include dateline
+    points in a computation of gradients
+    """
+    v0 = v.sel(longitude=0.)
+    v0['longitude']=360.
+    return xr.concat([v,v0],dim='longitude')
+
+#
+def mom_inv(dvdx, dvdy, f, o, r=1./20./86400.):
+    """ Inverse a linearized momentum equation
+    """
+    j = np.complex(0.,1.)
+    _o = o + j*r
+    uc = -g * (j*_o*dvdx - f*dvdy)/(_o**2-f**2)
+    vc = -g * (f*dvdx + j*_o*dvdy)/(_o**2-f**2)
     return uc, vc
 
 def ri2c(r,i):
@@ -43,3 +60,35 @@ def ri2c(r,i):
 
 def c2ri(c):
     return np.real(c), np.imag(c)
+
+#------------------------------ bathymetry ---------------------------------------
+
+def load_bathy(b='etopo2'):
+    ''' Load bathymetry
+    '''
+    interp = False
+    #
+    if b is 'etopo2':
+        #bfile = '/home2/pharos/othr/aponte/bathy/ETOPO2v2c_f4.nc'
+        bfile = './ETOPO2v2c_f4.nc'
+        bathy = xr.open_dataset(bfile)
+        #nc = Dataset(bfile)
+        #lonb = nc.variables['x'][:]
+        #latb = nc.variables['y'][:]
+        #hb = nc.variables['z'][:]
+        #nc.close()
+        interp = True
+    if interp:
+        from scipy.interpolate import interp2d, RectBivariateSpline
+        #lonb, latb = np.meshgrid(lonb,latb)
+        #self.h = interp2d(lonb, latb, hb.T, kind='linear')(self.lon,self.lat)
+        iroll = np.where(lonb<0)[0][-1]+1
+        lonb = np.roll(lonb,-iroll)
+        hb = np.roll(hb,-iroll,axis=1)
+        lonb[lonb<0] = lonb[lonb<0] + 360.
+        return RectBivariateSpline(lonb, latb, hb.T, kx=1, ky=1)(self.lon,self.lat).T
+    else:
+        return hb
+        
+    
+        
