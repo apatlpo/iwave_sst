7c5b7ba7e7cd618d85e0e1c813c17219f3769273
Aurelien @LPO <aurelien.junk@gmail.com>
2018-03-02 12:06:26 +0100

    Add load functions

--- tides/utils.py
+++ tides/utils.py
@@ -8,9 +8,84 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 #from cmocean import cm
 import time
+from utide._ut_constants import ut_constants as utide
 
 g=9.81
 cpd=2.*np.pi/86400. # radian/s
+
+#------------------------------ HRET reader -------------------------------------
+
+def get_hret_ssh(constituents=['M2','N2','S2','K1','O1','P1'], lonb=None, latb=None, 
+            hret='./Carrere_HRET_testing.nc', bathy=False):
+    ''' Load HRET ssh
+    
+    Parameters
+    ----------
+    consituents: list of str
+        Consistuents considered
+    lonb: None, list, tuple
+        Bounds for longitude, e.g.: lonb=(10.,100.)
+    latb: None, list, tuple
+        Bounds for latitude, e.g.: latb=(-10.,10.)
+    hret: str
+        Path to HRET netcdf file
+    '''
+    hret = xr.open_dataset(hret, chunks={'longitude': 500, 'latitude': 500})
+    hret_constituents = ['M2','N2','S2','K1','O1','P1']
+    for c in hret_constituents:
+        if c not in constituents:
+            del hret[c+'re']
+            del hret[c+'im']        
+    #
+    omega = dict()
+    for cst,o in zip(utide['const']['name'], utide['const']['freq']):
+        if cst in constituents:
+            omega[cst] = o*24. # cpd, input frequencies are cph
+            print(cst+' omega=%e rad/s, %.3f cpd'%(o*2.*np.pi/3600., o*24.))
+    #
+    if lonb is not None:
+        # should handle 360 wrapping
+        hret = hret.where(hret['longitude']>=lonb[0], drop=True)
+        hret = hret.where(hret['longitude']<=lonb[1], drop=True)
+    if latb is not None:
+        # should check conventions for longitude
+        hret = hret.where(hret['latitude']>=latb[0], drop=True)
+        hret = hret.where(hret['latitude']<=latb[1], drop=True)
+    #
+    if bathy:
+        h = load_bathy(lon=hret.longitude, lat=hret.latitude)
+        hret = hret.assign(h=h)
+    #
+    return hret, constituents, omega
+
+
+def get_hret_uv(**kwargs):
+    ''' Load HRET currents
+    
+    Parameters
+    ----------
+    consituents: list of str
+        Consistuents considered
+    lonb: None, list, tuple
+        Bounds for longitude, e.g.: lonb=(10.,100.)
+    latb: None, list, tuple
+        Bounds for latitude, e.g.: latb=(-10.,10.)
+    hret: str
+        Path to HRET netcdf file
+    '''
+    hret, constituents, omega = get_hret_ssh(**kwargs)
+    #
+    omega_K1 = 15.04107*np.pi/180./3600. # deg/h -> rad/s
+    print(omega_K1)
+    f = 2*omega_K1*cpd*np.sin(np.pi/180.*hret['latitude'])
+    #
+    U = xr.Dataset()
+    V = xr.Dataset()
+    for cst in constituents:
+        eta = ri2c(hret[cst+'re'], hret[cst+'im'])
+        detadx_c, detady_c = grad(eta)
+        U[cst], V[cst] = mom_inv(detadx_c, detady_c, f, omega[cst]*cpd)
+    return U, V, constituents, omega
 
 #------------------------------ gradients ---------------------------------------
 
@@ -71,10 +146,12 @@ def lon_extension(v):
     """ Extends data array longitudinally in order to include dateline
     points in a computation of gradients
     """
-    v0 = v.sel(longitude=0.)
-    v0['longitude']=360.
-    return xr.concat([v,v0],dim='longitude')
-
+    if v['longitude'].min()==0. and v['longitude'].max()==359.95:
+        v0 = v.sel(longitude=0.)
+        v0['longitude']=360.
+        return xr.concat([v,v0],dim='longitude')
+    else:
+        return v
 #
 def mom_inv(dvdx, dvdy, f, o, r=1./20./86400.):
     """ Inverse a linearized momentum equation
