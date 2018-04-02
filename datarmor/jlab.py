e8736085a98cd0825785857697fb6023724811ce
Aurelien PONTE <aponte@datarmor1.ib0.ice.ifremer.fr>
2018-04-02 10:00:22 +0000

    further clean up, fix dashboard with openqueue

--- datarmor/jlab.py
+++ datarmor/jlab.py
@@ -6,15 +6,23 @@ import subprocess
 if __name__ == '__main__':
 
     jlab_port = '8877'
+    dash_port = '8787'
 
+    notebook_dir = os.environ['HOME']
     user = os.environ['USER']
 
+    host = socket.gethostname() # where jlab will be running
     hostname = 'datarmor1-10g'
 
-    host = socket.gethostname()
+    cmd = ['jupyter', 'lab', '--ip', host, 
+           '--no-browser', '--port', jlab_port, 
+           '--notebook-dir', notebook_dir]
+    print(' '.join(cmd))
+    proc = subprocess.Popen(cmd)
 
-    com = ['jupyter', 'lab', '--ip', host, '--no-browser','--port', jlab_port]
-    print(' '.join(com))
-    proc = subprocess.Popen(com)
-    print(f'ssh -N -L {jlab_port}:{host}:{jlab_port}  {user}@{hostname}')
+    print(f'ssh -N -L {jlab_port}:{host}:{jlab_port} '
+          f'-L {dash_port}:{host}:8787 {user}@{hostname}')
+    print('Then open the following URLs:')
+    print(f'\tJupyter lab: http://localhost:{jlab_port}')
+    print(f'\tDask dashboard: http://localhost:{dash_port}', flush=True)
 
